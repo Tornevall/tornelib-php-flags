cf9759d9d55e73bd77d9959c28e67a088eb4d426
Tomas Tornevall <thorne@tornevall.net>
2020-02-15 00:55:31 +0100

    First version of the external flagsetter.

--- src/Flags.php
+++ src/Flags.php
@@ -2,54 +2,151 @@
 
 namespace TorneLIB;
 
+/**
+ * Class Flags
+ * @package TorneLIB
+ * @version 6.0.0
+ */
 abstract class Flags
 {
-    private static $internalFlags;
+	private static $internalFlags;
 
-    /**
-     * Set internal flag parameter.
-     *
-     * @param string $flagKey
-     * @param string $flagValue Nullable since 6.0.10 = If null, then it is considered a true boolean, set
-     *     setFlag("key") will always be true as an activation key
-     *
-     * @return bool If successful
-     * @throws \Exception
-     * @since 6.0.0
-     */
-    public static function setFlag($flagKey = '', $flagValue = null)
-    {
-        if (!empty($flagKey)) {
-            if (is_null($flagValue)) {
-                $flagValue = true;
-            }
-            self::$internalFlags[$flagKey] = $flagValue;
+	/**
+	 * Set internal flag parameter.
+	 *
+	 * @param string $flagKey
+	 * @param string $flagValue Nullable since 6.0.10 = If null, then it is considered a true boolean, set
+	 *     setFlag("key") will always be true as an activation key
+	 *
+	 * @return bool If successful
+	 * @throws \Exception
+	 * @since 6.0.0
+	 */
+	public static function setFlag($flagKey = '', $flagValue = null)
+	{
+		if (!empty($flagKey)) {
+			if (is_null($flagValue)) {
+				$flagValue = true;
+			}
+			self::$internalFlags[$flagKey] = $flagValue;
 
-            return true;
-        }
+			return true;
+		}
 
-        // LIB_UNHANDLED
-        throw new \Exception(
-            "Flags can not be empty",
-            65535
-        );
-    }
+		// LIB_UNHANDLED
+		throw new \Exception(
+			"Flags can not be empty",
+			65535
+		);
+	}
 
-    /**
-     * Get internal flag
-     *
-     * @param string $flagKey
-     *
-     * @return mixed|null
-     * @since 6.0.0
-     */
-    public function getFlag($flagKey = '')
-    {
-        if (isset($this->internalFlags[$flagKey])) {
-            return $this->internalFlags[$flagKey];
-        }
+	/**
+	 * Get internal flag
+	 *
+	 * @param string $flagKey
+	 *
+	 * @return mixed|null
+	 * @since 6.0.0
+	 */
+	public static function getFlag($flagKey = '')
+	{
+		if (isset(self::$internalFlags[$flagKey])) {
+			return self::$internalFlags[$flagKey];
+		}
 
-        return null;
-    }
+		return null;
+	}
+
+
+	/// Flag sanities
+
+	/**
+	 * Check if flag is set and true
+	 *
+	 * @param string $flagKey
+	 *
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public static function isFlag($flagKey = '')
+	{
+		if (self::hasFlag($flagKey)) {
+			return (self::getFlag($flagKey) === 1 || self::getFlag($flagKey) === true ? true : false);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if there is an internal flag set with current key
+	 *
+	 * @param string $flagKey
+	 *
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public static function hasFlag($flagKey = '')
+	{
+		if (!is_null(self::getFlag($flagKey))) {
+			return true;
+		}
+
+		return false;
+	}
+
+
+	/// Cleanup Start
+
+	/**
+	 * @param string $flagKey
+	 *
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public static function unsetFlag($flagKey = '')
+	{
+		if (self::hasFlag($flagKey)) {
+			unset(self::$internalFlags[$flagKey]);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param string $flagKey
+	 *
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public static function removeFlag($flagKey = '')
+	{
+		return self::unsetFlag($flagKey);
+	}
+
+	/**
+	 * @param string $flagKey
+	 *
+	 * @return bool
+	 * @since 6.0.0
+	 */
+	public static function deleteFlag($flagKey = '')
+	{
+		return self::unsetFlag($flagKey);
+	}
+
+	/**
+	 * @since 6.0.0
+	 */
+	public static function clearAllFlags()
+	{
+		self::$internalFlags = [];
+	}
+
+	public static function getAllFlags()
+	{
+		return self::$internalFlags;
+	}
 
 }
--- tests/flagTests.php
+++ tests/flagTests.php
@@ -3,11 +3,53 @@
 namespace TorneLIB;
 
 if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    die();
+	die();
 }
+
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-class flagTests extends \PHPUnit_Framework_TestCase
-{
+use PHPUnit\Framework\TestCase;
 
+
+class flagTests extends TestCase
+{
+	/**
+	 * @test
+	 * @throws \Exception
+	 */
+	public function setFlagKey()
+	{
+		$flagStatus = Flags::setFlag('firstFlag', 'present');
+		static::assertTrue((
+		$flagStatus ? true : false &&
+			Flags::getFlag('firstFlag') === 'present'
+		));
+	}
+
+	/**
+	 * @test
+	 */
+	public function setTrueFlag() {
+		Flags::setFlag('secondFlag', true);
+		static::assertTrue(Flags::getFlag('secondFlag'));
+	}
+
+	/**
+	 * @test
+	 */
+	public function setTrueWitoutKey() {
+		Flags::setFlag('thirdFlag');
+		static::assertTrue(Flags::isFlag('thirdFlag'));
+	}
+
+	/**
+	 * @test
+	 */
+	public function getAll() {
+		Flags::setFlag('firstFlag', 'present');
+		Flags::setFlag('secondFlag', true);
+		Flags::setFlag('thirdFlag');
+
+		static::assertCount(3, Flags::getAllFlags());
+	}
 }
